d5629d7f735b4f6d31e74a0b81b3d9ec120c79c5
Tadej <tadej.dev@gmail.com>
2025-04-24 15:23:55 +0200

    fix: docker repository name casing in publish script

--- backend/publish.sh
+++ backend/publish.sh
@@ -1,4 +1,4 @@
 ./build-image.sh
 # TODO: we should probably properly version docker images
-docker tag nft-studio:latest Apillon/nft-studio:latest
-docker push Apillon/nft-studio:latest
+docker tag nft-studio:latest apillon/nft-studio:latest
+docker push apillon/nft-studio:latest
